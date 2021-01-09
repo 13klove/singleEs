93c491af901e8e3425b7f2e000a16ef46c13ca26
hbjang <13klove@naver.com>
2021-01-09 21:47:39 +0900

    update

--- src/main/java/com/es/sample/demo/config/JpaConfig.java
+++ src/main/java/com/es/sample/demo/config/JpaConfig.java
@@ -1,13 +1,11 @@
 package com.es.sample.demo.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-//@EntityScan(basePackages = {"com.skel.pro.common"})
 @EnableJpaRepositories(basePackages = "com.es.sample.demo", excludeFilters = { @ComponentScan.Filter(type = FilterType.REGEX, pattern = { ".*EsRepository" })})
 public class JpaConfig {
 }
